1fdce2ec38fac8d138051809dfa3cd7bd154d0ba
DinaraM <blueberrywitch@yandex.ru>
2025-05-16 11:47:46 +0300

    Remove unused imports in Main class for improved code cleanliness

--- src/main/java/dika/Main.java
+++ src/main/java/dika/Main.java
@@ -2,9 +2,6 @@ package dika;
 
 import dika.conventer.CSVToModelConverter;
 import dika.service.PhoneService;
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
